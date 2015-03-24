c2484ff39a6dcfab090b5787aadbd7bed96e1920
Konstantin Tarkus <hello@tarkus.me>
2015-03-24 17:02:01 +0300

    Add a gulp task which embeds data from JSON files into HTML output

---
+++ README.md
@@ -0,0 +1,9 @@
+## Sandbox Repository
+
+```
+$ gulp build --channel=default
+```
+
+```
+$ gulp build --channel=test
+```
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var argv = require('minimist')(process.argv.slice(2));
+
+// Load content from JSON files
+var data = (function() {
+  var fs = require('fs');
+  var _ = require('lodash');
+
+  var channelId = argv.channel || 'default';
+  var channels = require('./data/channels.json');
+  var channel = _.find(channels, function(_) {
+     return _.id === channelId;
+  });
+
+  return {channel: channel};
+}());
+
+gulp.task('default', function() {
+  return gulp.src('src/index.html')
+    .pipe($.replace(
+      '<!--settings-->',
+      'var App = App || {};' +
+      'App.channel = ' + JSON.stringify(data.channel) + ';'))
+    .pipe(gulp.dest('build'));
+});
